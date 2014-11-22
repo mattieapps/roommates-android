96361ae577a581d9e0b869ce97d39281646a9c7a
Andrew Mattie <admin@mattieapps.com>
2014-11-21 23:15:48 -0500

    New BaseActivity

---
+++ app/src/main/java/com/mattieapps/roommates/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.mattieapps.roommates;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by andrewmattie on 11/19/14.
+ */
+public class BaseActivity extends ActionBarActivity {
+}
