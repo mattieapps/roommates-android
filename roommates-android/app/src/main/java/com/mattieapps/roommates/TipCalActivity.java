74de7f20205b2b3455bf1e0bd0c933f3bdfdb1c3
Andrew Mattie <admin@mattieapps.com>
2014-11-23 21:29:51 -0500

    Revert 41637ab..b4467f5
    This rolls back to commit 41637ab04a93bac695536513f20f9e06be71c3b4.

--- roommates-android/app/src/main/java/com/mattieapps/roommates/TipCalActivity.java
+++ roommates-android/app/src/main/java/com/mattieapps/roommates/TipCalActivity.java
@@ -5,11 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.Theme;
@@ -19,12 +15,8 @@ import com.afollestad.materialdialogs.Theme;
  */
 public class TipCalActivity extends BaseActivity {
 
-    EditText mCheckAmountEditText, mGratuityEditText;
     Button mPickGratuityBtn;
-    ImageButton mSwitchFab, mCalculateTipFab;
-    TextView mOutputText;
-    RadioGroup mGratuityRadioGroup;
-    RadioButton mGratuityRadioButton;
+    ImageButton mSwitchFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +24,6 @@ public class TipCalActivity extends BaseActivity {
         setContentView(R.layout.activity_tips);
 
         mSwitchFab = (ImageButton) findViewById(R.id.tipsFabButton);
-        mCalculateTipFab = (ImageButton) findViewById(R.id.calculateTipFabButton);
-
-        mCheckAmountEditText = (EditText) findViewById(R.id.priceEditText);
-        mGratuityEditText = (EditText) findViewById(R.id.gratuityEditText);
-
-        mOutputText = (TextView) findViewById(R.id.outputTipTextView);
-
-        mGratuityRadioGroup = (RadioGroup) findViewById(R.id.gratuityRadioGroup);
 
         mSwitchFab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,7 +35,6 @@ public class TipCalActivity extends BaseActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             mSwitchFab.setBackground(getDrawable(R.drawable.circle_21));
-            mCalculateTipFab.setBackground(getDrawable(R.drawable.circle_21));
         }
 
         mPickGratuityBtn = (Button) findViewById(R.id.pickGratuityBtn);
@@ -61,31 +44,12 @@ public class TipCalActivity extends BaseActivity {
                 gratuityDialog();
             }
         });
-
-        mCalculateTipFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int gratuity = Integer.parseInt(mGratuityEditText.getText().toString());
-                int checkAmount = Integer.parseInt(mCheckAmountEditText.getText().toString());
-                int output = checkAmount * gratuity/100;
-                mOutputText.setText("Output:\n\n" + Integer.toString(output));
-            }
-        });
     }
 
     private void gratuityDialog() {
         new MaterialDialog.Builder(this)
                 .title("Gratuity amount")
                 .customView(R.layout.gratuity_dialog)
-                .callback(new MaterialDialog.Callback() {
-                    @Override
-                    public void onPositive(MaterialDialog materialDialog) {
-
-                    }
-
-                    @Override
-                    public void onNegative(MaterialDialog materialDialog) {}
-                })
                 .theme(Theme.LIGHT)  // the default is light, so you don't need this line
                 .positiveText("Set")  // the default for textual dialogs (not list or custom view dialogs) is 'OK'
                 .negativeText("Cancel")  // leaving this line out will remove the negative button
